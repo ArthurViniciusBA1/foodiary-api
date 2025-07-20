94cd91644279a0f2fbb211f475e462dd283edd41
Arthur <arthurnaocopia@gmail.com>
2025-07-20 15:57:37 -0300

    feat: Adiciona validação de dados com Zod nos controladores de autenticação e atualiza a configuração do Serverless

--- src/controllers/SignUpController.ts
+++ src/controllers/SignUpController.ts
@@ -1,10 +1,32 @@
+import z, { success } from 'zod';
 import { HttpRequest, HttpResponse } from '../types/Http';
-import { created } from '../utils/http';
+import { badRequest, created } from '../utils/http';
+
+const schema = z.object({
+  goal: z.enum(['loose', 'mantain', 'gain']),
+  gender: z.enum(['male', 'female']),
+  birthdate: z.iso.date(),
+  heigth: z.number(),
+  weight: z.number(),
+  activityLevel: z.number().min(1).max(5),
+  account: z.object({
+    name: z.string().min(1),
+    email: z.email(),
+    password: z.string().min(8)
+  })
+
+})
 
 export class SignUpController {
-  static async handle(request: HttpRequest): Promise<HttpResponse> {
+  static async handle({ body }: HttpRequest): Promise<HttpResponse> {
+    const { success, error, data } = schema.safeParse(body)
+
+    if (!success) {
+      return badRequest({errors: error.issues})
+    }
+
     return created({
-      accessToken: 'signup: token de acesso',
+      data: data
     });
   }
 }
